4e2e1ed0ba783c5202967502352c24a702dbef0d
Lucas <08.lucas.sanchez@gmail.com>
2023-03-13 01:01:38 +0100

    ajout de content racine / pour afficher page a propos

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,6 +5,7 @@ const Content = ({ pages, textContent }) => {
     return (
         <div className='content '>
             <Routes>
+                <Route path='' element={textContent.pagePrincipal} />
                 <Route path={pages.pagePrincipal} element={textContent.pagePrincipal} />
                 <Route path={pages.pageCv} element={textContent.pageCv} />
                 <Route path={pages.pageMesProjets} element={textContent.pageMesProjets} />
