c383a8603ae43efa6cb0a4df9e1fa94c9ae768d6
Lucas <08.lucas.sanchez@gmail.com>
2023-03-13 01:03:04 +0100

    value range input vers defaultValue

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -5,9 +5,12 @@ const TimelineItem = (props) => {
         <section>
             <h2>{props.titre}</h2>
             <ul>
-                <li>{props.description}</li>
+                <li><span>Description:</span> <br />{props.description}</li>
                 <li><span>Date</span> {props.date}</li>
                 <li><span>Langages</span> {props.langages}</li>
+                {props.lien &&
+                    <li><span><a href={props.lien} target="_blank"> Lien </a></span></li>
+                }
                 {/* <li><span>Billeterie</span> <a href="#">Lien</a></li> */}
             </ul>
         </section>
