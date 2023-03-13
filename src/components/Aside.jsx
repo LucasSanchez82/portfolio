8c3c9db0bdf52ab95bccdfdc07bbdc2536909a4a
Lucas <08.lucas.sanchez@gmail.com>
2023-03-14 00:13:55 +0100

    responsive

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Turn as Hamburger } from 'hamburger-react'
 
 const Aside = ({ pages }) => {
-    console.log(pages);
+    const [isOpen, setIsOpen] = useState(window.innerWidth > 786 ? true : Boolean)
     return (
         <aside>
-            <ul>
-                <li><Link to={pages.pagePrincipal}>A propos de moi</Link></li>
-                <li><Link to={pages.pageCv}>Mon CV</Link></li>
-                <li><Link to={pages.pageMesProjets}>Mes projets</Link></li>
-            </ul>
-        </aside>
+            {
+                <Hamburger size={28} onToggle={toggled => { setIsOpen(toggled) }} />
+            }
+
+            {
+                isOpen &&
+                < ul >
+                    <li><Link to={pages.pagePrincipal}>A propos de moi</Link></li>
+                    <li><Link to={pages.pageCv}>Mon CV</Link></li>
+                    <li><Link to={pages.pageMesProjets}>Mes projets</Link></li>
+                </ul>
+
+            }
+        </aside >
     );
 };
 
